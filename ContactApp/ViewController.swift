30704c55d3c0e40ef9560e18878ad6e9787dcfe9
yerkaam <eraorynbai@gmail.com>
2024-10-27 15:49:13 +0500

    Initial Commit

---
+++ ContactApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactApp
+//
+//  Created by Yerdaulet Orynbay on 27.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
